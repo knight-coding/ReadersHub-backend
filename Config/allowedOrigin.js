fd65b3889a553fb6ace2e04ded33cd3a0e2cb384
Manan Sehgal <manansehgal97@gmail.com>
2025-09-05 15:47:17 +0530

    config update

--- Config/allowedOrigin.js
+++ Config/allowedOrigin.js
@@ -3,7 +3,7 @@ const allowedOrigin = [
     'http://127.0.0.1:5500',
     'http://localhost:4000',
     'https://readershub-backend.onrender.com',
-    'https://readershub-frontend.vercel.app',
+    'https://readers-hub-frontend.vercel.app',
 ];
 
 module.exports = allowedOrigin;
